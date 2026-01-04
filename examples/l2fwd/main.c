1f7f568f42fce53ab2ce61e2a3b7059fca9fa953
Guofeng Li <liguofeng22s@ict.ac.cn>
2026-01-04 17:37:26 +0800

    l2fwd: modify default mbuf number

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -765,7 +765,7 @@ main(int argc, char **argv)
 	}
 
 	nb_mbufs = RTE_MAX(nb_ports * (nb_rxd + nb_txd + MAX_PKT_BURST +
-		nb_lcores * MEMPOOL_CACHE_SIZE), 8192U);
+		nb_lcores * MEMPOOL_CACHE_SIZE), 4096U);
 
 	/* Create the mbuf pool. 8< */
 	l2fwd_pktmbuf_pool = rte_pktmbuf_pool_create("mbuf_pool", nb_mbufs,
